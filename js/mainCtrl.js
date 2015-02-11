aeba97b1a37e698696bdb68ac90c96ce31293dd0
Steven Roper <steven.m.roper@gmail.com>
2015-02-10 20:52:25 -0700

    Finishing Step 3.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,5 +2,9 @@ var app = angular.module('quoteBook');
 
 app.controller('MainCtrl', function($scope, dataService) {
 
-	
+	var getQuotes = function() {
+		$scope.quotes = dataService.getData();
+	};
+
+	getQuotes();
 });
